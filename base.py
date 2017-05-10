6689fc1d34abb07295fdaad868ff13f549a39db6
badgeekluck <harun.1996.187@gmail.com>
2017-05-11 00:42:04 +0300

    changed admin names

--- base.py
+++ base.py
@@ -4,61 +4,112 @@ from database import MongoDB
 import check,os
 from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
 
+
 Token="376593798:AAHMNABESGpXiFGiQ8Bg-0CnHc2EwyXD1hk"
+
 updater = Updater(token = Token)
+
 dispatcher = updater.dispatcher
+
 mongodb=MongoDB()
-admins=["utkucanbykl","vlademir92","HB"]
-users=["utkucanbykl","vlademir92","HB"]
+
+admins=["utkucanbykl","vlademir92","badgeekluck"]
+
+users=["utkucanbykl","vlademir92","badgeekluck"]
+
+
 def start(bot,update):
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Bot çalışıyor.")
 
+
 def hello(bot,update):
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Hello "+update.message.from_user.first_name)
 
+
 def echo(bot,update):
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Merhaba "+update.message.from_user.first_name)
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Kaynak atmak için /kaynak <link>")
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Bota Hello dedirtmek için /hello")
+
     bot.sendMessage(chat_id = update.message.chat_id, text="Url Listesi için /UrlList")
 
+
 def kaynak(bot,update):
+
     if(update.message.from_user.username not in users):
+
         bot.sendMessage(chat_id=update.message.chat_id,text="Kaynak eklemenize izin yok .\n@utkucanbykl ya da @vlademir92 'e mesaj atıp eklenmenizi istiyebilirsiniz .")
+
         return
+
     msg = update.message.text
+
     x = str(msg ).replace("/kaynak"," ")
+
     k =  x.split(" ")
+
     a=check.url(k[2])
+
     if (a == True):
+
         if(mongodb.Insert(x,update.message.from_user.first_name)==False):
+
             bot.sendMessage(chat_id=update.message.chat_id,text="zaten var")
+
             return 0
+
         bot.sendMessage(chat_id=update.message.chat_id , text=update.message.from_user.first_name +
                                                               "'nin Kaynağı Databaseye kaydettim")
+
         readme = open('README.md', 'a')
+
         x = str(update.message.text).replace("/kaynak", " ")
+
         readme.write("{}".format(update.message.from_user.first_name+" <li>" + x + "</li>"))
+
         readme.close()
+
         os.system("git add -A")
+
         os.system("git commit -m '" + update.message.from_user.first_name + " Link '")
+
         os.system("git push")
+
         bot.sendMessage(chat_id=update.message.chat_id, text="Url github'a eklendi.")
+
     else:
+
         bot.sendMessage(chat_id=update.message.chat_id,text="URL HATALI")
 
+
 def UrlList(bot,update):
+
     list=mongodb.UrlList()
+
     for i in range(len(list)):
+
         bot.sendMessage(chat_id=update.message.chat_id, text=list[i]["url"])
 
+
 def ekle(bot,update):
+
     if(update.message.from_user.username not in admins):
+
         bot.sendMessage(chat_id=update.message.chat_id,text="Üye eklemeye izniniz yok")
+
     else:
+
         msg=update.message.text
+
         msg=msg.replace("/ekle","",1)
+
         users.append(msg)
+
         bot.sendMessage(chat_id=update.message.chat_id,text="Üye eklendi")
 
 
--- database.py
+++ database.py
@@ -1,35 +1,65 @@
 from pymongo import MongoClient
 
+
 class MongoDB():
 
+
     def __init__(self):
+
         self.client = MongoClient()
+
         self.db = self.client["telegram"]
+
         self.collection = self.db["telegramcollection"]
 
+
+
     def Insert(self,url,user):
+
         urll={
+
             "url":url,
+
             "user":user
         }
+
         try:
+
             if(self.UrlCheck(url)):
+
                 self.collection.insert(urll)
+
                 return True
+
             else:
+
                 return False
+
         except:
+
             return False
+
 
     def UrlList(self):
+
         myresults = list(self.collection.find())
+
         return myresults
 
+
+
     def UrlCheck(self,url):
+
         sonuc=list(self.collection.find({"url":url}))
+
         if(url in sonuc):
+
             print("Var")
+
             return False
+
         else:
+
             print("Yok")
+
             return True
